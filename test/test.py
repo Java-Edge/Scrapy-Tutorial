e8911bb5b859ba1a0798e740ba5b57d120d1bb67
ShiShusheng <2855845850@qq.com>
2019-03-20 21:22:48 +0800

    正则表达式 更新 \s S 字符用法

--- test/test.py
+++ test/test.py
@@ -4,11 +4,10 @@ __author__ = 'JavaEdge'
 import re
 
 # 源字符串
-# line = "13236519711"
-line = "13sssssssss"
+line = "你ss好"
 
 # 正则匹配规则
-regex_str = "(1[34578][^1]{9})"
+regex_str = "(你\S好)"
 
 # 匹配结果
 match_obj = re.match(regex_str, line)
