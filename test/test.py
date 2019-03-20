56901f99243aff35810924d8c2900df591df60f1
ShiShusheng <2855845850@qq.com>
2019-03-20 21:10:34 +0800

    正则表达式 更新 [] 字符用法

--- test/test.py
+++ test/test.py
@@ -4,10 +4,11 @@ __author__ = 'JavaEdge'
 import re
 
 # 源字符串
-line = "JavaEdge1234"
+# line = "13236519711"
+line = "13sssssssss"
 
 # 正则匹配规则
-regex_str = "((JavaEdge|JavaEdge)1234)"
+regex_str = "(1[34578][^1]{9})"
 
 # 匹配结果
 match_obj = re.match(regex_str, line)
