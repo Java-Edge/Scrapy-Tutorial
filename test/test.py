24db7b7bba7a769945644270d563a833131456da
ShiShusheng <2855845850@qq.com>
2019-03-21 14:08:23 +0800

    正则表达式 更新 汉字编码 字符用法

--- test/test.py
+++ test/test.py
@@ -4,13 +4,13 @@ __author__ = 'JavaEdge'
 import re
 
 # 源字符串
-line = "你 好"
+# line = "你好"
+line = "study in 家里蹲大学"
 
 # 正则匹配规则
-regex_str = "(你\W好)"
+regex_str = ".*?([\u4E00-\u9FA5]+大学)"
 
 # 匹配结果
 match_obj = re.match(regex_str, line)
-
 if match_obj:
     print(match_obj.group(1))
